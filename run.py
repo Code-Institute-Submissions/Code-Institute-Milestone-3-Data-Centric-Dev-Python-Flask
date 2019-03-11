17f6f681fca0ce8695b5a2750c15ac0e134d828a
Tomas Kaiser <tomas.kaiser06@gmail.com>
2019-03-11 21:06:45 +0000

    Added connection to mongoDB Atlas

--- run.py
+++ run.py
@@ -1,12 +1,25 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+# Connection to MongoDB Atlas
+app.config["MONGO_DBNAME"] = "cookbook"
+app.config["MONGO_URI"] = os.getenv('MONGODB_URI_COOKBOOK')
 
-@app.route('/')
-def index():
-   return "Hello Cookbook App"
+mongo = PyMongo(app)
+
+
+@app.route("/")
+@app.route("/landing_page")
+def landing_page():
+   return render_template("landing.html", recipe_cards=mongo.db.recipe_cards.find())
 
 if __name__ == '__main__':
-   app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)
+   app.run(debug=True)
+
+'''
+host=os.environ.get('IP'), port=int(os.environ.get('PORT')),
+'''
