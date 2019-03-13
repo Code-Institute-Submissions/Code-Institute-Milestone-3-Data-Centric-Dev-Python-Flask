5687087ebcbd12718194445083e5908bd83fca97
Tomas Kaiser <tomas.kaiser06@gmail.com>
2019-03-13 23:35:04 +0000

    Added Bcrypt and method POST into landing_page

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 import os
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, session, request
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
@@ -13,14 +14,27 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
-@app.route("/landing_page")
+@app.route("/landing_page", methods=['GET', 'POST'])
 def landing_page():
+   if request.method == 'POST':
+      users = mongo.db.users
+      print(request.form["username"])
+      existing_user = users.find_one({"name" : request.form["username"]})
+
+      #if existing_user is None:
+      #  hashpass = Bcrypt.hashpw(request.form["password"])
+
+
    return render_template("landing.html", recipe_cards=mongo.db.recipe_cards.find())
 
 
-@app.route("/log_in")
-def log_in():
+@app.route("/login")
+def login():
+   if "username" in session:
+      return "You are logged in as " + session["username"]
+
    return render_template("login.html")
+
 
 if __name__ == '__main__':
    app.run(debug=True)
