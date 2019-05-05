cc74cb48491b213509ab17e8bca891c11b8898c2
Tomas Kaiser <tomas.kaiser06@gmail.com>
2019-05-05 16:42:45 +0100

    Added height 425px to cards

--- run.py
+++ run.py
@@ -418,4 +418,8 @@ def logout():
    return redirect(url_for('landing_page'))
 
 if __name__ == '__main__':
-   app.run(debug=False, host=os.environ.get('IP'), port=int(os.environ.get('PORT')))
+   app.run(debug=True)
+
+   """
+   host=os.environ.get('IP'), port=int(os.environ.get('PORT'))
+   """
